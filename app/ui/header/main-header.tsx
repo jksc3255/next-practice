bdff2c63cf0e506cad8f7773b9dfe6a7ceb73948
kjh <jksc3255@naver.com>
2024-02-21 17:03:23 +0900

    sass(or scss) 적용

--- app/ui/header/main-header.tsx
+++ app/ui/header/main-header.tsx
@@ -1,25 +1,19 @@
+'use client';
+
 import { GNB } from '@/app/lib/define';
 import Link from 'next/link';
 import React from 'react';
+import styles from '@/app/ui/component.module.scss';
 
 const MainHeader = () => {
   return (
     <header>
-      <nav>
-        <ul style={{ display: 'flex' }}>
+      <nav className={styles.gnbNav}>
+        <ul style={{}}>
           {GNB.map((v) => {
             return (
-              <li key={`GNB_LINK_${v.link}`} style={{ flex: 1 }}>
-                <Link
-                  href={`${v.link}`}
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: 40,
-                    fontWeight: '500',
-                  }}
-                >
+              <li key={`GNB_LINK_${v.link}`}>
+                <Link href={`${v.link}`} className={styles.gnbLink}>
                   {v.title}
                 </Link>
               </li>
