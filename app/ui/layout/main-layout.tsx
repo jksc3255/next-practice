6d81c055e2a525b3c368a149a8465a0e08f0d8d3
kjh <jksc3255@naver.com>
2024-03-21 16:58:28 +0900

    Refactoring <MainLayout>

--- app/ui/layout/main-layout.tsx
+++ app/ui/layout/main-layout.tsx
@@ -2,10 +2,10 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import styles from '@/app/ui/component.module.scss';
 import classNames from 'classnames/bind';
 import { GNB } from '@/app/lib/define';
+import { usePathname } from 'next/navigation';
 
 const cx = classNames.bind(styles);
 
